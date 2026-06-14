c2d1ee33f94cb97a1bfad315b1b8aeaf587f808f
renattussupbayev-coder <renat.tussupbayev@gmail.com>
2026-06-14 13:09:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 
 st.set_page_config(page_title="Brawl Memory", layout="wide")
 
-CARD_BACK = "https://upload.wikimedia.org/wikipedia/en/5/5c/Brawl_Stars_logo.png"
+CARD_BACK = "https://i.imgur.com/6QZ9X1Q.png"
 
 BRAWLERS = [
     {"id": 16000000, "name": "Shelly"},
